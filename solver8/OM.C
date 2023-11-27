91cf49ed9fd364e4d57b6496d3f3f99d90b2f310
ArvindChouhan <101119473+ArvindChouhan@users.noreply.github.com>
2023-11-27 18:07:48 +0100

    Add files via upload

---
+++ solver8/OM.C
@@ -0,0 +1,115 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2019 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    laplacianFoam
+
+Group
+    grpBasicSolvers
+
+Description
+    Laplace equation solver for a scalar quantity.
+
+    \heading Solver details
+    The solver is applicable to, e.g. for thermal diffusion in a solid.  The
+    equation is given by:
+
+    \f[
+        \ddt{T}  = \div \left( D_T \grad T \right)
+    \f]
+
+    Where:
+    \vartable
+        T     | Scalar field which is solved for, e.g. temperature
+        D_T   | Diffusion coefficient
+    \endvartable
+
+    \heading Required fields
+    \plaintable
+        T     | Scalar field which is solved for, e.g. temperature
+    \endplaintable
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "fvOptions.H"
+#include "simpleControl.H"
+#include "Random.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    argList::addNote
+    (
+        "Laplace equation solver for a scalar quantity."
+    );
+
+    #include "postProcess.H"
+
+    #include "addCheckCaseOptions.H"
+    #include "setRootCaseLists.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    simpleControl simple(mesh);
+
+    #include "createFields.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+ 
+
+    Cle = C0/(U-k*U+1); 
+
+   
+    while (simple.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;  
+        
+        #include "Initialize_Variables.H"
+
+        #include "PhaseEq.H"
+        
+        #include "CEq.H"
+       
+        Info<< "Cle min " << min(Cle).value()<< " , Cle Max: " << max(Cle).value() << nl << endl;
+        Info<< "P Min: " << min(P).value()<< " , P Max: " << max(P).value() << nl << endl; 
+        Info<< "U Min: " << min(U).value()<< " , U Max: " << max(U).value() << nl << endl;
+        Info<< "Up Min: " << min(Up).value()<< " , Up Max: " << max(Up).value() << nl << endl;
+        Info<< "C Min: " << min(C).value()<< " , C Max: " << max(C).value() << nl << endl;
+        Info<< "Curvature Min: " << min(Curvature).value()<< " , Curvature Max: " << max(Curvature).value() << nl << endl;
+        #include "write.H"
+        
+        runTime.printExecutionTime(Info);
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
